9d3959659472c2ef80e2530b840cbdcdb7dc59d3
Eduardo <archivo.ehv@gmail.com>
2020-09-15 23:06:48 -0300

    actualiz 150920 prueba carga bd

--- app/src/main/java/com/example/prueba_intermedia_kotlin_v1/FirstFragment.kt
+++ app/src/main/java/com/example/prueba_intermedia_kotlin_v1/FirstFragment.kt
@@ -1,25 +1,47 @@
 package com.example.prueba_intermedia_kotlin_v1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.prueba_intermedia_kotlin_v1.database.OrderTab
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    lateinit var viewModel: OrderTabViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OrderTabViewModel::class.java)
+
+
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
+        //val order = OrderTab(1,"carne",4,3000)
+        //viewModel.insertOrderTab(orderTab = order)
+
+        //viewModel.allOrderTab.observe(viewLifecycleOwner, Observer {
+        //Log.d("PBA",it.toString())
+//})
+
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
